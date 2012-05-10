ba5d031d9fdb69ae40e303cdd89f668fbcc1b07b
Pierre <pierre.mallinjoud@gmail.com>
2012-05-10 12:23:48 +0200

    On a tout passé dans les fichiers de configurations

--- lib/Format.pm
+++ lib/Format.pm
@@ -3,38 +3,19 @@ use strict;
 use warnings;
 use Exporter qw(import);
 
-our @EXPORT = qw(get_base_fasterdb get_organism_adaptor get_table_sondes
-	get_table_intensites get_table_dabg get_table_transcription get_table_ni
-	get_table_si get_table_splicing get_table_carac_entites get_table_prom
-	get_table_polya date traduction_type);
+our @EXPORT = qw(get_organism_adaptor get_table_intensites get_table_dabg
+	get_table_transcription get_table_ni get_table_si get_table_splicing
+	date traduction_type);
 
 # ==============================================================================
 # Formatage commun a tout les scripts
 # ==============================================================================
-
-sub get_base_fasterdb{
-
-	my($organism) = @_;
-
-	return ($organism eq 'human') ? 'fasterdb_humain' : 'fasterdb_souris';
-
-}
 
 sub get_organism_adaptor{
 
 	my($organism) = @_;
 
 	return ($organism eq 'human') ? 'Human' : 'Mouse';
-
-}
-
-sub get_table_sondes{
-
-	my($organism) = @_;
-
-	return ($organism eq 'human')
-		? 'humain_probes_status'
-		: 'souris_probes_status';
 
 }
 
@@ -83,36 +64,6 @@ sub get_table_splicing{
 	my($id_project, $id_analyse) = @_;
 
 	return '_' . $id_project . '__' . $id_analyse . '_splicing';
-
-}
-
-sub get_table_carac_entites{
-
-	my($organism) = @_;
-
-	return ($organism eq 'human')
-		? 'humain_carac_entites'
-		: 'souris_carac_entites';
-
-}
-
-sub get_table_prom{
-
-	my($organism) = @_;
-
-	return ($organism eq 'human')
-		? 'humain_prom'
-		: 'souris_prom';
-
-}
-
-sub get_table_polya{
-
-	my($organism) = @_;
-
-	return ($organism eq 'human')
-		? 'humain_polya'
-		: 'souris_polya';
 
 }
 
